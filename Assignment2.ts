3eb043b27167a9227c49499ea519df4d0e9dca86
abhi16apr <65789373+abhi16apr@users.noreply.github.com>
2022-10-12 22:25:12 -0700

    Add files via upload

---
+++ Assignment2.ts
@@ -0,0 +1,87 @@
+// Assignment :- Create the array of customer which have two properties:-> id, name
+// Note:- perform all respective operation mentioned below :-
+// 1. createCustomer()
+// 2. UpdateCustomer(id);
+// 3. deleteCustomer(id);
+// 4. listCustomer();
+
+// Note :-- Write the Code in typescript with switch cases perform various operations
+// Ref: mdn array
+
+// declaring array in TS
+//let myArray:string[]=["Vlad", "Abhi", "Glenn"];
+// console.log(typeof(myArray));
+// console.log(myArray);
+
+// let myArray1:any[]=[1.2, "Amy", 1, "Delhi"];
+// console.log(typeof(myArray1));
+// console.log(myArray1);
+
+// console.log(typeof(customer));
+// console.log(customer);
+
+// creating the constructor
+class Customer {
+    constructor(public name: string, public id: number){
+    }
+}
+
+// creating the array 
+let customers: Customer[] = [];
+
+function createCustomer(id: number, name: string)
+{
+    let customer = new Customer(name, id);
+    customers.push(customer);
+    console.log("Adding customer and ID ---> " + customer);
+}
+
+ function updateCustomer(id: number, name: string)
+ {
+     console.log("Updating customer --> ", name);
+     customers.find(x=>x.id === id)!.name=name;
+
+ }
+
+ function deleteCustomer(id: number) 
+ {
+   console.log("deleting the customer from database ");
+   let indexd = customers.findIndex(x=>x.id === id);
+   customers.splice(indexd, 1);
+ }
+
+ function listCustomer()
+ {
+    for( let x of customers){
+        console.log(x);
+    }
+ }
+
+ console.log(createCustomer(20, " Edward"));
+ console.log(createCustomer(25, "Kristina"));
+ //console.log(updateCustomer(25));
+// console.log(deleteCustomer(22));
+ console.log(listCustomer());
+console.log(updateCustomer(20, "Edward updated"));
+console.log(listCustomer());
+// deleteCustomer(25);
+console.log(listCustomer());
+
+let flag : number=1
+ switch(flag) {
+      case 0:
+          console.log(createCustomer(20, "Edward"));
+          break;
+
+      case 1:
+          console.log(updateCustomer(25, "Kristina"));
+          break;
+
+      case 2:
+          console.log(listCustomer());
+          break;
+
+      case 3:
+          console.log(deleteCustomer(20));
+          break;
+  }
